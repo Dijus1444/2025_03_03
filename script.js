6d8aa5047ea9d7486b9001a0e94d9d005b0665c5
Linas1981 <navickas1981@gmail.com>
2025-03-03 11:17:10 +0200

    Ikeltas darbas

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-const
+const black1 = document.getElementById("black2");
+black1.style.backgroundColor = "black";
+black1.style.color = "white";
+
+const black2 = document.getElementById("black1");
+black2.style.backgroundColor = "black";
+black2.style.color = "white";
+
+const red2 = document.getElementById("red1");
+red2.style.backgroundColor = "red";
+
+const red3 = document.getElementById("red2");
+red3.style.backgroundColor = "red";
+
+const white1 = document.getElementById("white2");
+white1.style.color = "white";
+white1.style.backgroundColor = "green";
